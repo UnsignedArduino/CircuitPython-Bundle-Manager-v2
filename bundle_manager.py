b0f2b1438fcee0dc400b0ce42267c151fae44d58
UnsignedArduino <UnsignedArduino@outlook.com>
2021-10-02 15:42:41 -0400

    Can save GitHub token to OS' credential manager

---
+++ bundle_manager.py
@@ -0,0 +1,9 @@
+from credential_manager import CredentialManager
+
+SERVICE_NAME = "CircuitPython Bundle Manager v2"
+GITHUB_TOKEN_NAME = "github_token"
+
+
+class BundleManager:
+    def __init__(self):
+        self.cred_manager = CredentialManager(SERVICE_NAME, GITHUB_TOKEN_NAME)
---
+++ credential_manager.py
@@ -0,0 +1,68 @@
+import logging
+from typing import Union
+
+import keyring
+
+from create_logger import create_logger
+
+logger = create_logger(name=__name__, level=logging.DEBUG)
+
+
+class CredentialManager:
+    def __init__(self, service_name: str, github_token_name: str):
+        self.service_name = service_name
+        self.github_token_name = github_token_name
+        self.github_token = None
+
+    def set_github_token(self, token: str, save_in_keyring: bool = True):
+        """
+        Set the GitHub token that is used.
+
+        :param token: The token to set.
+        :param save_in_keyring: Whether to save it in the OS' credential
+         manager.
+        """
+        self.github_token = token
+        if save_in_keyring:
+            logger.debug("Saving token to OS' credential manager")
+            keyring.set_password(self.service_name, self.github_token_name,
+                                 token)
+
+    def get_github_token(self) -> Union[str, None]:
+        """
+        Get the GitHub token that is used. If one is already set in memory,
+        we will use that, otherwise we will try to load it from the OS'
+        credential manager, otherwise None.
+
+        :return: A str or None.
+        """
+        logger.debug("Obtaining GitHub token")
+        if self.github_token is not None:
+            return self.github_token
+        else:
+            return keyring.get_password(self.service_name,
+                                        self.github_token_name)
+
+    def has_github_token(self, in_keyring: bool = False) -> bool:
+        """
+        Get whether we have a GitHub token in memory or stored in the OS'
+        credential manager.
+
+        :param in_keyring: Whether to check only the OS' credential manager
+         for a token.
+        :return: A bool.
+        """
+        if in_keyring:
+            return keyring.get_password(self.service_name,
+                                        self.github_token_name) is not None
+        else:
+            return self.github_token is not None or \
+                   keyring.get_password(self.service_name,
+                                        self.github_token_name) is not None
+
+    def delete_github_token(self):
+        """
+        Delete the GitHub token from the OS' credential manager.
+        """
+        logger.debug("Deleting GitHub token")
+        keyring.delete_password(self.service_name, self.github_token_name)
