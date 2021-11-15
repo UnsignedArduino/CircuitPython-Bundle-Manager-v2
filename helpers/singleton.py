29d0487d504b2f2dc83ec57f44d5f1aa3a17a2f4
UnsignedArduino <UnsignedArduino@outlook.com>
2021-11-15 18:11:16 -0500

    Add license thing to top of every source file

--- helpers/singleton.py
+++ helpers/singleton.py
@@ -1,4 +1,25 @@
+"""
+CircuitPython Bundle Manager v2 - a Python program to easily manage
+modules on a CircuitPython device!
+
+Copyright (C) 2021 UnsignedArduino
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 # Found https://stackoverflow.com/a/6798042/10291933
+
 
 class Singleton(type):
     _instances = {}
